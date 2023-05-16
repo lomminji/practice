b6d8d596c36be76ee49f9b9a1291d139b1836d80
lomminji <128569983+lomminji@users.noreply.github.com>
2023-05-16 06:25:02 +0000

    조건문 끝

---
+++ missionnnnnnn.c
@@ -0,0 +1,17 @@
+// #include <stdio.h>
+// main(){
+//   int x;
+//   printf("90이상:A | 80이상:B | 70이상:C | 60이상:D | 60미만:F\n");
+//   scanf("%d", &x);
+
+//   if(x>=90)
+//     printf("A");
+//   else if(x>=80)
+//     printf("B");
+//   else if(x>=70)
+//     printf("C");
+//   else if(x>=60)
+//     printf("D");
+//   else if(x<60)
+//     printf("F");
+// }
